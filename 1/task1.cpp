45582ebad2b166d3a5f8b55b176423d3e0487915
liza583 <liza30102006@mail.ru>
2025-12-12 21:37:26 +0300

    Create task1.cpp

---
+++ 1/task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Введите имя: ";
+    string user;
+    cin >> user;
+    cout << "Привет " << user << " !"<< endl;
+}
